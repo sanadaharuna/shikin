ae55ea923a4492734216fe0ccbb3d07d24af9260
hirai katsuyuki <hirai@adm.niigata-u.ac.jp>
2020-04-09 17:38:48 +0900

    comment out csv_export of urls.py

--- src/erad/urls.py
+++ src/erad/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import EradListView, erad_scraping, export_csv
+from .views import EradListView, erad_scraping
 
 app_name = "erad"
 urlpatterns = [
     path("list", EradListView.as_view(), name="list"),
     path("scraping", erad_scraping, name="scraping"),
-    path("export_csv", export_csv, name="export_csv"),
+    # path("export_csv", export_csv, name="export_csv"),
 ]
