cb31c1b49009e60417438e7fa206ed5de765a248
hirai katsuyuki <hirai@adm.niigata-u.ac.jp>
2020-06-08 17:24:49 +0900

    revert models

--- src/erad/models.py
+++ src/erad/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 
 class Item(models.Model):
-    id = models.CharField("整理番号", max_length=255)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
-    url = models.URLField("配分機関公募情報URL", max_length=255, unique=True)
+    # メタデータ
+    url = models.URLField("配分機関公募情報URL", max_length=200, unique=True)
+    # reference_number = models.CharField("整理番号", max_length=200, unique=True)
+    # indexにある項目
     publishing_date = models.DateField("公開日")
-    funding_agency = models.CharField("配分機関", max_length=255)
-    call_for_applications = models.CharField("公募名", max_length=255)
-    application_unit = models.CharField("応募単位", max_length=255)
-    approved_institution = models.CharField("機関承認の有無", max_length=255)
+    funding_agency = models.CharField("配分機関", max_length=200)
+    call_for_applications = models.CharField("公募名", max_length=200)
+    application_unit = models.CharField("応募単位", max_length=200)
+    approved_institution = models.CharField("機関承認の有無", max_length=200)
     opening_date = models.DateTimeField("受付開始日")
     closing_date = models.DateTimeField("受付終了日")
 
